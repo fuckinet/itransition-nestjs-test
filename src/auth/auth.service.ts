b14083853d09c66a54ceeaeee4bbe7951607a893
Nikita Khlyustin <nikita.ser777@gmail.com>
2021-06-18 16:33:21 +0300

    unused code remove

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
