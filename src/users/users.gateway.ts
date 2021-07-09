98cef8de46bd5889b0410f483c45426c1ead48dd
Nikita Khlyustin <nikita.ser777@gmail.com>
2021-07-09 12:10:17 +0300

    users gateway example with publish message to channel add

--- src/users/users.gateway.ts
+++ src/users/users.gateway.ts
@@ -9,7 +9,19 @@ import { UsersService } from './users.service';
 
 @WebSocketGateway(8000)
 export class UsersGateway {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {
+    setInterval(() => {
+      (async () => {
+        try {
+          // Publish data; wait for an acknowledgement from the back end broker (if it exists).
+          await this.server.exchange.invokePublish('testChannel', 'This is some more data');
+          console.log('success publish with ack a message');
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    }, 2000);
+  }
   @WebSocketServer()
   server: AGServer;
 
